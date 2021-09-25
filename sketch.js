40648397c3eeae839c0c3a23fc2bc8c14721fc9b
Guneet410 <85295632+Guneet410@users.noreply.github.com>
2021-09-25 12:48:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -332,7 +332,7 @@ text("Score : "+ rScore,520,100)
 
 
 function spikeSpawn(){
-  spike  = createSprite(windowWidth+10,windowHeight/2 +250,20,10);
+  spike  = createSprite(windowWidth+10,windowHeight/2 +300,20,10);
   spike.velocityX = -8 ;
   spike.addImage("spikes",spikes);
   spike.scale =0.3
@@ -341,7 +341,7 @@ function spikeSpawn(){
 }
 
 function stoneSpawn(){
-  stones = createSprite(windowWidth+10,windowHeight/2 +250,20,10);
+  stones = createSprite(windowWidth+10,windowHeight/2 +300,20,10);
   stones.addImage("stone",stone)
   stones.velocityX = -10;
   StGroup.add(stones); 
